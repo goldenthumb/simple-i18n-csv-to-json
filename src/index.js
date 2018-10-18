40ac6db1e78a8325da3a77a2cef43622a0c5031e
yhchoi <cyh8914@gmail.com>
2018-10-18 14:20:28 +0900

    fix csv to array

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const fs = require('fs-extra');
 
 module.exports = async (filePath) => {
   const csv = await fs.readFileSync(filePath, { encoding: 'utf8' });
-  const data = csv.split(/\r?\n/).map(str => str.split(','));
+
+  if (!csv) {
+    throw new Error('File Does Not Exist.');
+  }
+
+  const data = csvToArray(csv);
   const locales = data.shift().slice(1);
   const output = {};
 
@@ -16,4 +21,32 @@ module.exports = async (filePath) => {
   });
 
   return output;
+};
+
+const csvToArray = (data) => {
+  if (data.slice(-1) !== '\n') data += '\n';
+
+  const regex = /("(?:[^"]|"")*"|[^,"\n\r]*)(,|\r?\n|\r)/y;
+  const rows = [];
+  let row = [];
+
+  while (true) {
+    const match = data.match(regex);
+
+    if (!match) {
+      return rows;
+    }
+
+    const [, str, separator] = match;
+    const value = str[0] !== '"' ? str : str.slice(1, -1).replace(/""/g, '"');
+
+    if (separator === ',' || value) {
+      row.push(value);
+    }
+
+    if (separator !== ',') {
+      rows.push(row);
+      row = [];
+    }
+  }
 };
